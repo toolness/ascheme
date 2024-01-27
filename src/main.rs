e4a2eea1d2db8d6e4f1912bf5f2d5c332b9a0d26
Atul Varma <varmaa@gmail.com>
2024-01-26 23:31:43 -0500

    Add a simple parser.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,26 @@
-use crate::{string_interner::StringInterner, tokenizer::Tokenizer};
+use crate::{
+    parser::{Expression, Parser},
+    string_interner::StringInterner,
+    tokenizer::Tokenizer,
+};
 
+mod parser;
 mod string_interner;
 mod tokenizer;
 
 fn main() {
-    let mut k = Tokenizer::new(&"  (  ) ");
+    let code = "  (+ 1 2 (* 3 4)) ";
+    let mut k = Tokenizer::new(&code);
     let mut interner = StringInterner::default();
+    let parser = Parser::new(code, Tokenizer::new(&code), &mut interner);
+    let parsed = parser.collect::<Result<Vec<Expression>, _>>();
 
     let boop1 = interner.intern("boop");
     let boop2 = interner.intern("boop");
     let bap = interner.intern("bap");
 
     println!(
-        "Hello, world! {:?} {:?} {boop1:?} {boop2:?} {bap:?} {}",
+        "Hello, world! {:?} {:?} {boop1:?} {boop2:?} {bap:?} {} {parsed:?}",
         k.next(),
         k.next(),
         interner.get(boop1)
