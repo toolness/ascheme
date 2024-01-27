231f9ec210d2422079b043da6476298327603eae
Atul Varma <varmaa@gmail.com>
2024-01-27 14:37:20 -0500

    Factor out eval_expression().

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -26,50 +26,50 @@ pub struct Interpreter<'a> {
 }
 
 impl<'a> Interpreter<'a> {
+    fn eval_expression(&mut self, expression: &Expression) -> Result<Value, RuntimeError> {
+        match &expression.0 {
+            ExpressionValue::Number(number) => Ok(Value::Number(*number)),
+            ExpressionValue::Symbol(_) => {
+                // TODO: Look up the symbol in the environment and return its value, if possible.
+                return Err(RuntimeErrorType::UnboundVariable.source_mapped(expression.1));
+            }
+            ExpressionValue::Combination(expressions) => {
+                let Some(operator) = expressions.get(0) else {
+                    return Err(RuntimeErrorType::IllFormedExpression.source_mapped(expression.1));
+                };
+                match operator.0 {
+                    ExpressionValue::Number(_) => {
+                        return Err(RuntimeErrorType::InvalidOperator.source_mapped(operator.1))
+                    }
+                    ExpressionValue::Symbol(symbol) => {
+                        let add = self.interner.intern("+");
+                        let multiply = self.interner.intern("*");
+                        if symbol == add {
+                            // TODO: Implement addition!
+                            Ok(Value::Undefined)
+                        } else if symbol == multiply {
+                            // TODO: Implement multiplication!
+                            Ok(Value::Undefined)
+                        } else {
+                            // TODO: Look up the symbol in the environment.
+                            return Err(RuntimeErrorType::UnboundVariable.source_mapped(operator.1));
+                        }
+                    }
+                    ExpressionValue::Combination(_) => {
+                        return Err(RuntimeErrorType::Unimplemented(
+                            "TODO: Implement combinations for operators",
+                        )
+                        .source_mapped(operator.1))
+                    }
+                }
+            }
+        }
+    }
+
     fn eval(&mut self) -> Result<Value, RuntimeError> {
         let mut last_value: Value = Value::Undefined;
         for expression in self.expressions {
-            last_value = match &expression.0 {
-                ExpressionValue::Number(number) => Value::Number(*number),
-                ExpressionValue::Symbol(_) => {
-                    // TODO: Look up the symbol in the environment and return its value, if possible.
-                    return Err(RuntimeErrorType::UnboundVariable.source_mapped(expression.1));
-                }
-                ExpressionValue::Combination(expressions) => {
-                    let Some(operator) = expressions.get(0) else {
-                        return Err(
-                            RuntimeErrorType::IllFormedExpression.source_mapped(expression.1)
-                        );
-                    };
-                    match operator.0 {
-                        ExpressionValue::Number(_) => {
-                            return Err(RuntimeErrorType::InvalidOperator.source_mapped(operator.1))
-                        }
-                        ExpressionValue::Symbol(symbol) => {
-                            let add = self.interner.intern("+");
-                            let multiply = self.interner.intern("*");
-                            if symbol == add {
-                                // TODO: Implement addition!
-                                Value::Undefined
-                            } else if symbol == multiply {
-                                // TODO: Implement multiplication!
-                                Value::Undefined
-                            } else {
-                                // TODO: Look up the symbol in the environment.
-                                return Err(
-                                    RuntimeErrorType::UnboundVariable.source_mapped(operator.1)
-                                );
-                            }
-                        }
-                        ExpressionValue::Combination(_) => {
-                            return Err(RuntimeErrorType::Unimplemented(
-                                "TODO: Implement combinations for operators",
-                            )
-                            .source_mapped(operator.1))
-                        }
-                    }
-                }
-            };
+            last_value = self.eval_expression(expression)?;
         }
         Ok(last_value)
     }
