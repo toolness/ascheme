4c37ff3e7eeb101514a93fbd1ccf1eb927445f56
Atul Varma <varmaa@gmail.com>
2024-01-28 18:50:01 -0500

    Refactor environment to store globals separately.

--- src/environment.rs
+++ src/environment.rs
@@ -2,34 +2,37 @@ use std::collections::HashMap;
 
 use crate::{interpreter::Value, string_interner::InternedString};
 
+type Bindings = HashMap<InternedString, Value>;
+
 #[derive(Default)]
 pub struct Environment {
-    symbol_stack: Vec<HashMap<InternedString, Value>>,
+    globals: Bindings,
+    lexical_scope_stack: Vec<Bindings>,
 }
 
 impl Environment {
     pub fn push(&mut self) {
-        self.symbol_stack.push(HashMap::new());
+        self.lexical_scope_stack.push(HashMap::new());
     }
 
     pub fn pop(&mut self) {
-        self.symbol_stack.pop();
+        self.lexical_scope_stack.pop();
     }
 
     pub fn get(&self, identifier: &InternedString) -> Option<&Value> {
-        for symbol_map in self.symbol_stack.iter().rev() {
-            if let Some(value) = symbol_map.get(identifier) {
+        for bindings in self.lexical_scope_stack.iter().rev() {
+            if let Some(value) = bindings.get(identifier) {
                 return Some(value);
             }
         }
-        None
+        self.globals.get(identifier)
     }
 
     pub fn set(&mut self, identifier: InternedString, value: Value) {
-        if self.symbol_stack.is_empty() {
-            self.push();
+        if let Some(bindings) = self.lexical_scope_stack.last_mut() {
+            bindings.insert(identifier, value);
+        } else {
+            self.globals.insert(identifier, value);
         }
-        let symbols = self.symbol_stack.last_mut().unwrap();
-        symbols.insert(identifier, value);
     }
 }
