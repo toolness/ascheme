9cdb410bae6c06ac7799a1a5d120365b7e051763
Atul Varma <varmaa@gmail.com>
2024-02-10 15:15:12 -0500

    Start making the tracked objects notify the tracker of being dropped.

--- src/object_tracker.rs
+++ src/object_tracker.rs
@@ -1,17 +1,27 @@
 use core::fmt::Debug;
 use std::{
+    cell::RefCell,
     ops::Deref,
     rc::{Rc, Weak},
 };
 
+struct TrackedInner<T>(T, Weak<RefCell<ObjectTrackerInner<T>>>, usize);
+
+impl<T> Drop for TrackedInner<T> {
+    fn drop(&mut self) {
+        // TODO: Tell the tracker to forget about us.
+        if let Some(tracker) = self.1.upgrade() {}
+    }
+}
+
 #[derive(Clone)]
-pub struct Tracked<T>(Rc<T>);
+pub struct Tracked<T>(Rc<TrackedInner<T>>);
 
 impl<T> Deref for Tracked<T> {
     type Target = T;
 
     fn deref(&self) -> &Self::Target {
-        &self.0
+        &self.0 .0
     }
 }
 
@@ -20,42 +30,49 @@ where
     T: Debug,
 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_tuple("Tracked").field(&self.0).finish()
+        f.debug_tuple("Tracked").field(&self.0 .0).finish()
     }
 }
 
 impl<T: PartialEq> PartialEq for Tracked<T> {
     fn eq(&self, other: &Self) -> bool {
-        self.0 == other.0
+        self.0 .0 == other.0 .0
     }
 }
 
-pub struct ObjectTracker<T> {
-    objects: Vec<Weak<T>>,
+struct ObjectTrackerInner<T> {
+    objects: Vec<Option<Weak<TrackedInner<T>>>>,
 }
+
+impl<T> ObjectTrackerInner<T> {
+    fn track(&mut self, object: T, weak_self: Weak<RefCell<Self>>) -> Tracked<T> {
+        let id = self.objects.len();
+        let rc = Rc::new(TrackedInner(object, weak_self, id));
+        self.objects.push(Some(Rc::downgrade(&rc)));
+        Tracked(rc)
+    }
+}
+
+pub struct ObjectTracker<T>(Rc<RefCell<ObjectTrackerInner<T>>>);
 
 impl<T> Default for ObjectTracker<T> {
     fn default() -> Self {
-        Self { objects: vec![] }
+        let inner = ObjectTrackerInner { objects: vec![] };
+        Self(Rc::new(RefCell::new(inner)))
     }
 }
 
 impl<T> ObjectTracker<T> {
     pub fn track(&mut self, object: T) -> Tracked<T> {
-        let rc = Rc::new(object);
-        self.objects.push(Rc::downgrade(&rc));
-        Tracked(rc)
+        let weak_self = Rc::downgrade(&self.0);
+        self.0.borrow_mut().track(object, weak_self)
     }
 
     pub fn compact(&mut self) {
-        let objects = std::mem::take(&mut self.objects);
-        self.objects = objects
-            .into_iter()
-            .filter(|weakref| weakref.upgrade().is_some())
-            .collect();
+        // TODO: We don't need this anymore, it can be removed I think.
     }
 
     pub fn len(&self) -> usize {
-        self.objects.len()
+        self.0.borrow().objects.len()
     }
 }
