a13269725f4e65293e7aaaaa71e8fd74e1bb99f1
Atul Varma <varmaa@gmail.com>
2024-02-10 21:18:25 -0500

    blah

--- src/gc.rs
+++ src/gc.rs
@@ -6,6 +6,20 @@ pub struct Visitor {
 }
 
 impl Visitor {
+    /// This is pretty frustrating--all I want is a unique identifier for "the thing being traversed"
+    /// so I can make sure I don't loop infinitely while traversing the object graph,
+    /// but this appears to be impossible because some of the things we're traversing are
+    /// actually at the same memory location as different things we're traversing (e.g.,
+    /// the first item of a struct is actually at the same memory location as
+    /// the struct itself).
+    ///
+    /// We might be able to disambiguate by the vtable of the dyn pointer, but Rust doesn't
+    /// actually seem to give us access to that, and I can't store the raw pointer either,
+    /// because then Rust complains about lifetime issues, so ... I guess I am just going to
+    /// additionally pass in a string that represents the "type" of thing being traversed and
+    /// use that as part of the identifier.
+    ///
+    /// This feels extremely stupid but I don't know what else to do.
     pub fn traverse(&self, traverser: &dyn Traverser, type_id: &'static str) {
         let traverser_ptr = (traverser as *const dyn Traverser) as *const () as usize;
         let type_id_ptr = (type_id as *const str) as *const () as usize;
