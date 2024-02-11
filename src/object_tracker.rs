0bb5b5273a144c75c5e31b98b7aecdf3b5f28c28
Atul Varma <varmaa@gmail.com>
2024-02-11 06:23:31 -0500

    println -> eprintln

--- src/object_tracker.rs
+++ src/object_tracker.rs
@@ -20,13 +20,13 @@ impl<T: CycleBreaker> Drop for TrackedInner<T> {
             if let Ok(mut tracker) = tracker.try_borrow_mut() {
                 tracker.untrack(self.id);
             } else {
-                println!(
+                eprintln!(
                     "WARNING: Unable to untrack object #{} (tracker.borrow_mut() failed).",
                     self.id
                 );
             }
         } else {
-            println!(
+            eprintln!(
                 "WARNING: Unable to untrack object #{} (tracker does not exist).",
                 self.id
             );
