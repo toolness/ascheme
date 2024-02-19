a4ddd7f85a966aa42b04affd307b9fe0520b2eff
Atul Varma <varmaa@gmail.com>
2024-02-19 11:29:32 -0500

    Factor out MutableString::from_tokenized_source().

--- src/mutable_string.rs
+++ src/mutable_string.rs
@@ -8,6 +8,31 @@ impl MutableString {
         MutableString(Rc::new(RefCell::new(value)))
     }
 
+    pub fn from_tokenized_source(repr: &str) -> Self {
+        let mut chars: Vec<char> = Vec::with_capacity(repr.len());
+        // The `skip(1)` skips the opening quote.
+        let mut is_escaped = false;
+        for char in repr.chars().skip(1) {
+            if is_escaped {
+                if char == 'n' {
+                    chars.push('\n');
+                } else {
+                    chars.push(char);
+                }
+                is_escaped = false;
+            } else {
+                if char == '\\' {
+                    is_escaped = true;
+                } else {
+                    chars.push(char);
+                }
+            }
+        }
+        chars.pop(); // Remove closing quote.
+        let string: String = chars.into_iter().collect();
+        Self::new(string)
+    }
+
     pub fn points_at_same_memory_as(&self, other: &MutableString) -> bool {
         &*self.0 as *const RefCell<String> == &*other.0 as *const RefCell<String>
     }
