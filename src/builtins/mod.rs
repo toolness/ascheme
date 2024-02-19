4c945173ef4fa4205337bbc28c139c301b2c3e8c
Atul Varma <varmaa@gmail.com>
2024-02-19 15:43:14 -0500

    Move tests into builtins/mod.rs.

--- src/builtins/mod.rs
+++ src/builtins/mod.rs
@@ -211,3 +211,135 @@ fn display(ctx: ProcedureContext) -> ProcedureResult {
     ctx.interpreter.printer.print(format!("{:#}", value));
     Ok(Value::Undefined.into())
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::test_util::{test_eval_success, test_eval_successes};
+
+    #[test]
+    fn quote_works() {
+        test_eval_success("(quote 1)", "1");
+        test_eval_success("(quote (1   2    3  ))", "(1 2 3)");
+        test_eval_success("(quote (1 2 3 (4)))", "(1 2 3 (4))");
+        test_eval_success("(quote #t)", "#t");
+        test_eval_success("(quote #f)", "#f");
+        test_eval_success("(quote ())", "()");
+        test_eval_success("(quote blarg)", "blarg");
+
+        test_eval_success("'1", "1");
+        test_eval_success("'(1   2    3  )", "(1 2 3)");
+        test_eval_success("'(1 2 3 (4))", "(1 2 3 (4))");
+        test_eval_success("'#t", "#t");
+        test_eval_success("'#f", "#f");
+        test_eval_success("'()", "()");
+        test_eval_success("'blarg", "blarg");
+    }
+
+    #[test]
+    fn set_car_works() {
+        test_eval_success("(define a (quote (1 . 2))) (set-car! a 5) a", "(5 . 2)");
+    }
+
+    #[test]
+    fn set_cdr_works() {
+        test_eval_success("(define a (quote (1 . 2))) (set-cdr! a 5) a", "(1 . 5)");
+    }
+
+    #[test]
+    fn cond_works() {
+        test_eval_success("(cond (1))", "1");
+        test_eval_success("(cond (0))", "0");
+        test_eval_success("(cond (#f))", "");
+        test_eval_success("(cond (1 2 3 4))", "4");
+        test_eval_success("(cond (#f 1) (else (+ 1 1)))", "2");
+        test_eval_success("(cond (1) (lolol))", "1");
+    }
+
+    #[test]
+    fn variable_definitions_work() {
+        test_eval_success("(define x 3) x", "3");
+        test_eval_success("(define x 3) (define y (+ x 1)) (+ x y)", "7");
+    }
+
+    #[test]
+    fn compound_procedure_definitions_work() {
+        test_eval_success("(define (x) 3)", "");
+        test_eval_success("(define (x) 3) (x)", "3");
+        test_eval_success("(define (add-three n) (+ 3 n)) (add-three 1)", "4");
+    }
+
+    #[test]
+    fn lambda_definitions_work() {
+        test_eval_success("(define x (lambda () 3))", "");
+        test_eval_success("(define x (lambda () 3)) (x)", "3");
+        test_eval_success("(define add-three (lambda (n) (+ 3 n))) (add-three 1)", "4");
+    }
+
+    #[test]
+    fn set_works_with_globals() {
+        test_eval_success("(define x 1) (set! x 2) x", "2");
+        test_eval_success("(define x 1) (set! x (+ x 1)) x", "2");
+    }
+
+    #[test]
+    fn set_works_in_closures() {
+        test_eval_successes(&[
+            (
+                "
+                (define (make-incrementer)
+                  (define n 0)
+                  (lambda ()
+                    (set! n (+ n 1))
+                    n
+                  )
+                )
+                (define foo (make-incrementer)) 
+                (define bar (make-incrementer)) 
+                ",
+                "",
+            ),
+            ("(foo)", "1"),
+            ("(foo)", "2"),
+            ("(foo)", "3"),
+            ("(bar)", "1"),
+            ("(bar)", "2"),
+        ]);
+    }
+
+    #[test]
+    fn if_works() {
+        test_eval_success("(if #t 1)", "1");
+        test_eval_success("(if #t 1 2)", "1");
+        test_eval_success("(if #f 1 2)", "2");
+
+        // R5RS section 4.1.5 says this behavior is unspecified, we'll just return undefined.
+        test_eval_success("(if #f 1)", "");
+    }
+
+    #[test]
+    fn compound_procedues_prefer_argument_values_to_globals() {
+        test_eval_success(
+            "
+            (define n 5)
+            (define (add-three n) (+ 3 n))
+            (+ (add-three 1) n)
+        ",
+            "9",
+        );
+    }
+
+    #[test]
+    fn compound_procedues_use_lexical_scope() {
+        test_eval_success(
+            "
+            (define (make-adder n)
+              (define (add-n x) (+ x n))
+              add-n
+            )
+            (define add-three (make-adder 3))
+            (add-three 1)
+        ",
+            "4",
+        );
+    }
+}
